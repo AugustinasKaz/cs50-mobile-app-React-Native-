331fd54263451f7eb4d2390829fab930575f4f61
Kaziux <pelenyt3@gmail.com>
2019-07-28 22:13:27 +0300

    start and reset buttons added

--- project1/App.js
+++ project1/App.js
@@ -1,21 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Button} from 'react-native';
+import { Constants } from 'expo';
 
-export default class App extends React.Component {
+class Counter extends React.Component {
+  state = {
+    minutes: 24,
+    seconds: 60,
+  }
+  
+  componentDidMount() {
+    this.timer = setInterval(this.dicrease, 1000)
+  }
+  
+  componentWillUnmount(){
+    clearInterval(this.timer)
+  }
+  dicrease = () => {
+    this.setState(prevState => ({seconds: prevState.seconds - 1}))
+    if(this.state.seconds == 0){
+      this.setState(prevState => ({minutes: prevState.minutes - 1, seconds: 60}))
+    }
+  }
+  
   render() {
+    return <Text>{this.state.minutes} : {this.state.seconds}</Text>
+  }
+}
+
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      stop: true,
+    }
+  }
+
+  startCount = () => this.setState(prevState => ({ start: !prevState.start,}));
+
+  stopCount = () => 
+  render() {
+    if(this.state.start){
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Reset" onPress={this.startCount}/>
+      <Button title="Stop" onPress={this.stopCount}/>
+        <Counter />
       </View>
     );
+    }
+    else{
+      return(
+      <View style={styles.container}>
+      <Button title="Start" onPress={this.startCount}/>
+      </View>
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
   },
 });
