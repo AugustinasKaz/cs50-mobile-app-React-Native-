c8b0bc25c7fc329e36bb1aca83d01e2d7f69c263
Kaziux <pelenyt3@gmail.com>
2019-08-04 22:39:02 +0300

    axios added

--- project2/components/api.js
+++ project2/components/api.js
@@ -1,9 +1,11 @@
+import axios from 'axios'
 
 export const fetchData = async (data) => {
-  console.log(data);
-  //const response = await fetch('http://www.omdbapi.com/?apikey=9abbfc0&s='+data)
-  //const {results} = await response.json()
-  return 'Hello';
+  const promise = await axios.get('http://www.omdbapi.com/?apikey=9abbfc0&', {params: {s: data}})
+  //const status = promise.status;
+  //if(status === 200)
+  const results = promise.data;
+  return results;
 }
 
 
--- project2/components/home.js
+++ project2/components/home.js
@@ -3,20 +3,22 @@ import {StyleSheet,TextInput, View, Text} from 'react-native';
 import {fetchData} from './api.js'
 
 export default class Home extends React.Component{
-    state ={
+    constructor(){
+     super()
+     this.state ={
         search: '',
+     }
     }
-    update = e =>{
-        this.setState({search: e.target.value})
-        console.log(this.state.search);
+    update = (text) =>{
+        this.setState({search: text})
     }
-    test = () => {
-        fetchData(this.state.search).then(response => console.log(response))
-    } 
+    componentDidUpdate(){
+        fetchData(this.state.search).then(response => console.log(response.Title));           
+    }
     render(){
         return(
             <View>
-                <TextInput style={styles.input} value={this.state.search} onChange={this.update}/>
+                <TextInput style={styles.input} value={this.state.search} onChangeText={this.update}/>
             </View>
         );
     }
@@ -28,6 +30,10 @@ const styles = StyleSheet.create({
         borderWidth: 1,
           },
   });
+
+
+
+
 
 
 
